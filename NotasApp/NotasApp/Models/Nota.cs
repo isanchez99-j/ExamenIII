6cc3d8dff1a0ec6d77843223bc539f0eba3c684f
isanchez99-j <107815313+isanchez99-j@users.noreply.github.com>
2023-04-22 11:59:06 -0600

    commit

---
+++ NotasApp/NotasApp/Models/Nota.cs
@@ -0,0 +1,21 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+
+namespace NotasApp.Models
+{
+    [Table("notas")]
+    class Nota : BaseModel
+    {
+        [PrimaryKey("id")]
+        public int idNota { get; set; }
+        [Column("desc")]
+        public string desc { get; set; }
+        [Column("fecha")]
+        public string fecha { get; set; }
+        [Column("audioUrl")]
+        public string audioUrl { get; set; }
+        [Column("imgUrl")]
+        public string imgUrl { get; set; }
+    }
+}
+
---
+++ NotasApp/NotasApp/Notas.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Android.Media;
+using NotasApp.Models;
+
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace NotasApp
+{
+    public partial class Notas : ContentPage
+    {
+        public MediaPlayer player;
+        public Notas()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var Historial = await App.Supa.From<Nota>().Get();
+            ListaEmpleados.ItemsSource = Historial.Models;
+        }
+        private async void ListaEmpleados_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var d = e.SelectedItem as Nota;
+
+            Debug.WriteLine(d.fecha);
+            // await Navigation.PushAsync(pagina);
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            var button = (Button)sender;
+            var soundUrl = (string)button.CommandParameter;
+
+            // Create a new instance of the MediaPlayer class
+            player = new MediaPlayer();
+            player.Reset();
+            // Set the source of the audio file using the Source property
+            player.SetDataSource(soundUrl);
+            player.Prepare();
+            player.Start();
+        }
+
+        private void Button_Clicked_1(object sender, System.EventArgs e)
+        {
+            player.Stop();
+        }
+    }
+}
